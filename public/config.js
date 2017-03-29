e8979ade7ae28bddb909a31cd7e8d7cb55055404
JJ <jgg3295@gmail.com>
2017-03-28 20:14:28 -0700

    fix config

--- public/config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-	googleKey: 'AIzaSyD2n92u_k8M-PQJFAW2bdxqqG1EbQS7s5o',
-	email: 'makowski.sm@gmail.com',
-	fbId: '178117606038161',
-	fbSecret: '46b97d04d73253dcbcb443a6b3741ccb'
-};
